1947d145f28aae735f66c452b00cdc44ad625bd5
PM <121531128+26-PM@users.noreply.github.com>
2024-03-18 17:29:49 +0000

    Changed `next.config.mjs` to use `output: 'export'`.

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,26 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output:'export',
-// };
-// /* module.exports = nextConfig*/
-// export default nextConfig;
-
-
-
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output:'export',
+};
+/* module.exports = nextConfig*/
+export default nextConfig;
+
+
+
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   output: 'export',
  
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+//   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+//   // trailingSlash: true,
  
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
+//   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+//   // skipTrailingSlashRedirect: true,
  
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
+//   // Optional: Change the output directory `out` -> `dist`
+//   // distDir: 'dist',
+// }
  
-module.exports = nextConfig
+// module.exports = nextConfig
