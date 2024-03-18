4b61055f94699728cfe3bdc3b0ae28ca29c51325
PM <121531128+26-PM@users.noreply.github.com>
2024-03-18 21:55:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,27 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//       domains: ['images.unsplash.com', 'res.cloudinary.com']
-//   }
-// };
-
-// export default nextConfig;
-
-
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  images: {
+      domains: ['images.unsplash.com', 'res.cloudinary.com']
+  }
+};
+
+export default nextConfig;
+
+
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   output: 'export',
  
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+//   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+//   // trailingSlash: true,
  
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
+//   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+//   // skipTrailingSlashRedirect: true,
  
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
+//   // Optional: Change the output directory `out` -> `dist`
+//   // distDir: 'dist',
+// }
  
 module.exports = nextConfig
