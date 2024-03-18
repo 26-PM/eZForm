e82ed4a43c6262c918630b9140c8f728141fa48e
PM <121531128+26-PM@users.noreply.github.com>
2024-03-18 20:08:47 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
- /** @type {import('next').NextConfig} */
- const nextConfig = {
-   images: {
-       domains: ['images.unsplash.com', 'res.cloudinary.com']
-   }
- };
-
- export default nextConfig;
