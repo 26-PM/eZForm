40cef50be9fced4123ccb7d07d5ea53881652553
PM <121531128+26-PM@users.noreply.github.com>
2024-03-19 10:14:02 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:'export',
+    images: {
+        domains: ['images.unsplash.com', 'res.cloudinary.com']
+    }
 };
-/* module.exports = nextConfig*/
+
 export default nextConfig;
-
-
-
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   output: 'export',
- 
-//   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-//   // trailingSlash: true,
- 
-//   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-//   // skipTrailingSlashRedirect: true,
- 
-//   // Optional: Change the output directory `out` -> `dist`
-//   // distDir: 'dist',
-// }
- 
-// module.exports = nextConfig
