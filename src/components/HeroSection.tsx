5b525cc9409dfc9486733082f5627110203b655c
PM <121531128+26-PM@users.noreply.github.com>
2024-03-19 05:42:46 +0000

    Update HeroSection content

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,9 +12,12 @@ import { Spotlight } from "./ui/Spotlight"
       <div className="p-4 relative z-10 w-full text-center">
          <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 mb-4">eZ Form</h1>
          <h3 className="mt-20 md:mt-0 text-1xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 mb-4">By formFusion</h3>
-         <p>Lorem ipsum do lor, sit amet consectetur adipisicing elit. Inventore tempora esse ipsa deleniti optio eos, accusamus vel error ullam et velit in debitis dolores accusantium expedita a libero sed atque distinctio fugit magnam excepturi aliquid. Minima ducimus assumenda id voluptate, incidunt ea. Excepturi, odit. Minus doloribus, sequi deleniti iure doloremque incidunt tempora? Unde nam aut reiciendis, maiores numquam quis dolore aspernatur consequatur at velit dignissimos nisi illo rem explicabo quae cumque voluptatem soluta obcaecati. Obcaecati dolor laudantium possimus doloribus asperiores quisquam, ratione suscipit esse et consequuntur odio! Consequatur sapiente atque maiores, itaque nobis, sint ut magnam, quaerat dicta ab soluta?</p>
+         <p className="mt-20 font-medium">Worrying about filling form?</p>
+         <p className="font-semibold">Not again!</p>
+         <p className="font-bold">We are here to help you!</p>
+         <p className="font-extrabold">We'll take care of the forms for you</p>
       </div>
-      <div className="mt-4 ">
+      <div className="mt-4 font-black ">
         <Link href={"/courses"}>
           Explore
         </Link>
