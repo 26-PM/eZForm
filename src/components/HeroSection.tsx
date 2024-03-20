7f09dd794140d490e845a6409264fa85c9d63bb6
PM <121531128+26-PM@users.noreply.github.com>
2024-03-20 13:31:22 +0000

    Add Tailwind CSS color variables and update Hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,7 @@
-import Link from "next/link"
+ import Link from "next/link"
 import { Spotlight } from "./ui/Spotlight"
+import { Button } from "./ui/moving-border";
+
   function HeroSection() {
   return (
     <div
@@ -9,7 +11,7 @@ import { Spotlight } from "./ui/Spotlight"
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-      <div className="p-4 relative z-10 w-full text-center">
+      <div className="p-4 relative z-10 w-full text-center mt-20">
          <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 mb-4">eZ Form</h1>
          <h3 className="mt-20 md:mt-0 text-1xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 mb-4">By formFusion</h3>
          <p className="mt-20 font-medium">Worrying about filling form?</p>
@@ -17,9 +19,14 @@ import { Spotlight } from "./ui/Spotlight"
          <p className="font-bold">We are here to help you!</p>
          <p className="font-extrabold">We'll take care of the forms for you</p>
       </div>
-      <div className="mt-4 font-black ">
+      <div className="mt-20 font-black ">
         <Link href={"/courses"}>
-          Explore
+        <Button
+        borderRadius="1.75rem"
+        className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+      >
+        Explore More
+      </Button>
         </Link>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,22 @@
 import type { Config } from "tailwindcss";
 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+// Plugin to add each Tailwind color as a global CSS variable
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme('colors'));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -31,6 +48,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors ],
 };
 export default config;
