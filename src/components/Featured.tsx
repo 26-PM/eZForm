10d2ea0782a7adf03b014e889080ddebcd303dd1
PM <121531128+26-PM@users.noreply.github.com>
2024-03-24 07:39:27 +0000

    changed bg color of features section

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,27 +1,27 @@
 'use client';
 import React from 'react';
 import Link from 'next/link';
-import { Button } from "./ui/moving-border";
+// import { Button } from "./ui/moving-border";
 import { BackgroundGradient } from './ui/background-gradient';
 
 function Featured() {
   return (
-    <div className='py-12 bg-gray-950'>
+    <div className='py-12 '>
       <div>
         <div className="text-center">
           <h2 className="text-xl font-extrabold text-white sm:text-5xl">Features</h2>
           <p></p>
         </div>
       </div>
-      <div className='m-10 flex text-center justify-center gap-x-96 gap-y-36 flex-wrap font-semibold bg-black p-10 rounded-3xl'>
-        <BackgroundGradient><div className="flex justify-center p-6">Easy</div></BackgroundGradient>
-        <BackgroundGradient><div className="flex justify-center p-6">User-friendly</div></BackgroundGradient>
-        <BackgroundGradient><div className="flex justify-center p-6">Simple</div></BackgroundGradient>
-        <BackgroundGradient><div className="flex justify-center p-6">Seamless</div></BackgroundGradient>
-        <BackgroundGradient><div className="flex justify-center p-6">Hassle-free</div></BackgroundGradient>
-        <BackgroundGradient><div className="flex justify-center p-6">Streamlined</div></BackgroundGradient>
-        <BackgroundGradient><div className="flex justify-center p-6">Convenient</div></BackgroundGradient>
-        <BackgroundGradient><div className="flex justify-center p-6">Smooth</div></BackgroundGradient>
+      <div className='m-10 flex text-center justify-center gap-x-96 gap-y-14 flex-wrap font-semibold bg-black p-10 rounded-3xl'>
+        <BackgroundGradient><div className="flex justify-center p-8">Easy</div></BackgroundGradient>
+        <BackgroundGradient><div className="flex justify-center p-8">User-friendly</div></BackgroundGradient>
+        <BackgroundGradient><div className="flex justify-center p-8">Simple</div></BackgroundGradient>
+        <BackgroundGradient><div className="flex justify-center p-8">Seamless</div></BackgroundGradient>
+        <BackgroundGradient><div className="flex justify-center p-8">Hassle-free</div></BackgroundGradient>
+        <BackgroundGradient><div className="flex justify-center p-8">Streamlined</div></BackgroundGradient>
+        <BackgroundGradient><div className="flex justify-center p-8">Convenient</div></BackgroundGradient>
+        <BackgroundGradient><div className="flex justify-center p-8">Smooth</div></BackgroundGradient>
       </div>
       {/* <div className='mt-20 text-center'>
         <Link href={"/"}>
